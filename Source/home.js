e0005158c67643c212fc907676146b5feee5554b
Akash Shukla <akashshukla.mp@gmail.com>
2023-04-21 04:00:09 +0530

    First Commit After Creating Complete Website

---
+++ Source/home.js
@@ -0,0 +1,69 @@
+// console.log("Hello World");
+
+let head = document.getElementById("sthead");
+let dis = document.getElementById("stdis");
+let stpic = document.getElementById("stpic");
+// a.innerText = "Hello Para 1";
+
+let stbtn1 = document.getElementById("stbtn1");
+let stbtn2 = document.getElementById("stbtn2");
+let stbtn3 = document.getElementById("stbtn3");
+let stbtn4 = document.getElementById("stbtn4");
+let stbtn5 = document.getElementById("stbtn5");
+let stbtn6 = document.getElementById("stbtn6");
+let stbtn7 = document.getElementById("stbtn7");
+
+stbtn1.onclick= function(){
+    head.innerText = stbtn1.innerText;
+    stpic.classList = "st1pic";
+    dis.innerHTML = "Once upon a time, there lived a monkey in the jungle. He was very cunning, and all the animals of the jungle used to dislike him.";
+}
+
+
+stbtn2.onclick= function(){
+
+    head.innerText = stbtn2.innerText;
+    stpic.classList = "st2pic";
+    dis.innerHTML = "Once upon a time, there lived a lion in the jungle. He was very powerful and strong, and all the animals of the jungle feared him. The lion used to dominate and kill other animals in the jungle for his food, and sometimes, even unnecessarily.";
+
+}
+
+stbtn3.onclick= function(){
+
+    head.innerText = stbtn3.innerText;
+    stpic.classList = "st3pic";
+    dis.innerHTML = "Once upon a time, there lived a foolish lion.";
+
+}
+
+stbtn4.onclick= function(){
+
+    head.innerText = stbtn4.innerText;
+    stpic.classList = "st4pic";
+    dis.innerHTML = "Once upon a time, there lived a lion and a rabbit in a jungle. They were very good friends.";
+
+}
+
+stbtn5.onclick= function(){
+
+    head.innerText = stbtn5.innerText;
+    stpic.classList = "st5pic";
+    dis.innerHTML = "Once upon a time, there was a king who ruled a great and glorious nation. Favourite amongst his subjects was the court painter of whom he was very proud.";
+
+}
+
+stbtn6.onclick= function(){
+
+    head.innerText = stbtn6.innerText;
+    stpic.classList = "st6pic";
+    dis.innerHTML = "Once upon a time, there lived a queen in the city of Benaras. Her name was Khema and she was the wife of King Bahuputtaka. One night, the Queen had a dream of a beautiful golden deer that spoke with great wisdom, almost as if he was a sage.";
+
+}
+
+stbtn7.onclick= function(){
+
+    head.innerText = stbtn7.innerText;
+    stpic.classList = "st7pic";
+    dis.innerHTML = "The ant promised to help the dove. One day the dove was sitting on the same tree. There was a hunter. The hunter aimed at the deer.";
+
+}
